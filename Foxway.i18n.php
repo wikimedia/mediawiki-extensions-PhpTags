67749ab44e2315a6fa226b064fb555b89ca11a66
raymond <raimond.spekking@gmail.com>
2013-04-02 16:37:57 +0200

    Consistency tweaks in preparation for adding extension to Translatewiki.net
    Change-Id: I8644fc52b0cb89098eecaa51d8d683c90c0866f3

--- Foxway.i18n.php
+++ Foxway.i18n.php
@@ -13,15 +13,15 @@ $messages = array();
  * @author pastakhov
  */
 $messages['en'] = array(
-	'foxway-desc' => 'Allows to store an object-oriented data and implements its own runtime for php code on wikipage',
-	'foxway-php-syntax-error-unexpected' => 'PHP Parse error: syntax error, unexpected $1 in Command line code on line $2'
+	'foxway-desc' => 'Allows to store an object-oriented data and implements its own runtime for PHP code on pages',
+	'foxway-php-syntax-error-unexpected' => 'PHP parse error: syntax error, unexpected $1 in command line code on line $2'
 );
 
 /** Message documentation (Message documentation)
  * @author pastakhov
  */
 $messages['qqq'] = array(
-	'foxway-desc' => '{{desc|name=Foxway|url=http://www.mediawiki.org/wiki/Extension:Foxway}}',
+	'foxway-desc' => '{{desc|name=Foxway|url=https://www.mediawiki.org/wiki/Extension:Foxway}}',
 	'foxway-php-syntax-error-unexpected' => 'Error message, parameters:
 * $1 - token or user-specified string a quoted
 * $2 - the line number where the error occurred',
