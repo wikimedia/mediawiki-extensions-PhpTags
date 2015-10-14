81f8b644b58debc3bc18cf068844b6d28aa7b203
Pavel Astakhov <pastakhov@yandex.ru>
2015-10-14 10:13:28 +0600

    Fix test initialization
    Change-Id: I0710306dfcabaf997f3e7ac2cda760e92e3de3fa

--- tests/phpunit/includes/!!!firstInit_Test.php
+++ tests/phpunit/includes/!!!firstInit_Test.php
@@ -1,21 +1,23 @@
 <?php
-wfDebug( 'PHPTags: test initialization ' . __FILE__ );
-\PhpTags\Hooks::addJsonFile( __DIR__ . '/PhpTags_test.json' );
-const PHPTAGS_TEST = 'Test';
-const PHPTAGS_TEST_BANNED = 'Test';
-\Hooks::register( 'PhpTagsBeforeCallRuntimeHook', function ( $hookType, $objectName, $methodName, $values ) {
-	if ( $hookType === \PhpTags\Runtime::H_GET_CONSTANT || $hookType === \PhpTags\Runtime::H_GET_OBJECT_CONSTANT ) {
-		$methodName = strtolower( $methodName );
-	}
-	if ( substr( $methodName, -6 ) === 'banned' ) {
-		$hookTypeString = \PhpTags\Hooks::getCallInfo( \PhpTags\Hooks::INFO_HOOK_TYPE_STRING );
-		\PhpTags\Runtime::pushException( new \PhpTags\HookException( "Sorry, you cannot use this $hookTypeString" ) );
-		return false;
-	}
-	return true;
-} );
 
 if ( \PhpTags\Renderer::$needInitRuntime ) {
+	wfDebug( 'PHPTags: test initialization ' . __FILE__ );
+
+	\PhpTags\Hooks::addJsonFile( __DIR__ . '/PhpTags_test.json' );
+	define( 'PHPTAGS_TEST', 'Test' );
+	define( 'PHPTAGS_TEST_BANNED', 'Test' );
+	\Hooks::register( 'PhpTagsBeforeCallRuntimeHook', function ( $hookType, $objectName, $methodName, $values ) {
+		if ( $hookType === \PhpTags\Runtime::H_GET_CONSTANT || $hookType === \PhpTags\Runtime::H_GET_OBJECT_CONSTANT ) {
+			$methodName = strtolower( $methodName );
+		}
+		if ( substr( $methodName, -6 ) === 'banned' ) {
+			$hookTypeString = \PhpTags\Hooks::getCallInfo( \PhpTags\Hooks::INFO_HOOK_TYPE_STRING );
+			\PhpTags\Runtime::pushException( new \PhpTags\HookException( "Sorry, you cannot use this $hookTypeString" ) );
+			return false;
+		}
+		return true;
+	} );
+
 	wfDebug( 'PHPTags: run hook PhpTagsRuntimeFirstInit ' . __FILE__ );
 	\Hooks::run( 'PhpTagsRuntimeFirstInit' );
 	\PhpTags\Hooks::loadData();
