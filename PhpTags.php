d510a6b856bf5f91fb723088dcd8d4e18a580308
Pavel Astakhov <pastakhov@yandex.ru>
2016-08-01 11:08:14 +0600

    Commented out warning message about deprecated PHP entry point
    Change-Id: I42aabd703eb7b57ade289fded349bf57695063a5

--- PhpTags.php
+++ PhpTags.php
@@ -4,11 +4,11 @@ if ( function_exists( 'wfLoadExtension' ) ) {
 	// Keep i18n globals so mergeMessageFileList.php doesn't break
 	$wgMessagesDirs['PhpTags'] = __DIR__ . '/i18n';
 	$wgExtensionMessagesFiles['PhpTagsMagic'] = __DIR__ . '/PhpTags.i18n.magic.php';
-	wfWarn(
-		'Deprecated PHP entry point used for PhpTags extension. ' .
-		'Please use wfLoadExtension instead, ' .
-		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
-	);
+//	wfWarn(
+//		'Deprecated PHP entry point used for PhpTags extension. ' .
+//		'Please use wfLoadExtension instead, ' .
+//		'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
+//	);
 	return;
 } else {
 	die( 'This version of the PhpTags extension requires MediaWiki 1.25+' );
